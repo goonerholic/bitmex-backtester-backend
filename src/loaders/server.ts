1b1ef305e518918c55e70cb41e8d177bef1fbc9c
goonerholic <goonerholic@gmail.com>
2020-06-17 17:01:04 +0900

    fix express port link

--- src/loaders/server.ts
+++ src/loaders/server.ts
@@ -1,13 +1,16 @@
 import bodyParser from 'body-parser';
 import api from '../api';
 import { Application } from 'express';
+import { config } from '../config';
+
+const { port } = config;
 
 export default function serverLoader(app: Application) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/api', api);
 
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log('Server initialized.');
   });
   return app;
